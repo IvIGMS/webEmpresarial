79e38d8197b59f5ff2844938826b949271e3b9b0
Ivan <friasgilivan@gmail.com>
2022-07-15 13:40:43 +0200

    Fusionamos el fontend con el backend

--- webEmpresarial/webEmpresarial/urls.py
+++ webEmpresarial/webEmpresarial/urls.py
@@ -14,16 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
+    # Path Admin
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('services/', views.services, name='services'),
-    path('store/', views.store, name='store'),
-    path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog, name='blog'),
-    path('sample/', views.sample, name='sample'),
+    # Path Core
+    path('', include("core.urls"))
 ]
